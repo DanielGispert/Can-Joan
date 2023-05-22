a681a71ec51412f491a9f936029e6c23626229d7
DanielGispert <daniel.gispert@estudiantat.upc.edu>
2023-05-22 17:36:47 +0200

    creating form with java and html

---
+++ app.js
@@ -0,0 +1,10 @@
+document.getElementById("reservaForm").addEventListener("submit", function(event) {
+    event.preventDefault(); 
+
+    var ReservName = document.getElementById("ReservName"),value;
+    var ReservDate = document.getElementById("ReservName"),value;
+    var ReservTime = document.getElementById("ReservName"),value;
+    var NumberPeople = document.getElementById("ReservName"),value;
+    
+    document.getElementById("reservaForm").reset();
+});
