84fefcedbc35ed91139041219a6016429ab61c40
DanielGispert <daniel.gispert@estudiantat.upc.edu>
2023-05-22 17:56:19 +0200

    table out

--- app.js
+++ app.js
@@ -3,14 +3,12 @@ formulario.addEventListener( "submit", validarFormulario)
 function validarFormulario (e){
     e.preventDefault();
     const Name = document.querySelector("#ReservName").value
-    const Date = document.querySelector("#ReservName").value
+    const Date = document.querySelector("#ReservDate").value
     const Time = document.querySelector("#ReservTime").value
     const People = document.querySelector("#NumberPeople").value
 
-    //num rando between 0 i 15 per la taula escollida
-    let table = Number((Math.random() * 15).toFixed(0));
     const respuesta = document.getElementById("respuesta");
-    respuesta.textContent = `Hello, your reservation at number ${Name} of ${People} people on the day ${Date} on ${Time} is confirmed, thanks for counting on us. Table ${table} reserved.`
+    respuesta.textContent = `Hello, your reservation to name ${Name} of ${People} people on the day ${Date} on ${Time} is CONFIRMED, thanks for counting on us.`
 
 }
 
