88644596b7e13ac10dde2c574fe43f8d3a71ccfe
DanielGispert <daniel.gispert@estudiantat.upc.edu>
2023-05-22 17:52:10 +0200

    fORM AND RESPOND DONE

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
-document.getElementById("reservaForm").addEventListener("submit", function(event) {
-    event.preventDefault(); 
+const formulario = document.querySelector("#formulario")
+formulario.addEventListener( "submit", validarFormulario) 
+function validarFormulario (e){
+    e.preventDefault();
+    const Name = document.querySelector("#ReservName").value
+    const Date = document.querySelector("#ReservName").value
+    const Time = document.querySelector("#ReservTime").value
+    const People = document.querySelector("#NumberPeople").value
 
-    var ReservName = document.getElementById("ReservName"),value;
-    var ReservDate = document.getElementById("ReservName"),value;
-    var ReservTime = document.getElementById("ReservName"),value;
-    var NumberPeople = document.getElementById("ReservName"),value;
+    //num rando between 0 i 15 per la taula escollida
+    let table = Number((Math.random() * 15).toFixed(0));
+    const respuesta = document.getElementById("respuesta");
+    respuesta.textContent = `Hello, your reservation at number ${Name} of ${People} people on the day ${Date} on ${Time} is confirmed, thanks for counting on us. Table ${table} reserved.`
+
+}
+
+
     
-    document.getElementById("reservaForm").reset();
-});
